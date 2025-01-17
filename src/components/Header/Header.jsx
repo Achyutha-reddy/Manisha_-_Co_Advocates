88ad02a5a30195192b10af009c8593837a1cef39
unknown <achyuthareddyborampeta@gmail.com>
2025-01-17 20:59:27 +0530

    first commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from 'react-icons/fa'; // Social media icons
 import './Header.css'; // Create a CSS file for styling
+import logo from '../../assets/logo.png'
 
 const Header = () => {
   return (
     <header className="header">
       <div className="header-container">
+        {/* Logo and Title */}
         <div className="header-logo">
+          <img src={logo} alt="Logo" className="logo-image" />
           <h1 className="title">Manisha & Co Advocates</h1>
         </div>
 
+        {/* Social Media Icons */}
         <div className="header-socio">
           <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
             <FaTwitter size={24} />
